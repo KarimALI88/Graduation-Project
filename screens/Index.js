973eea212c829acf774a3fe9c826c675da172370
Karim Ali <88720700+KarimALI88@users.noreply.github.com>
2024-06-12 19:00:14 +0300

    last index till the discussion

--- screens/Index.js
+++ screens/Index.js
@@ -1,4 +1,10 @@
-import React, { useContext, useState, useRef, useEffect,useCallback } from "react";
+import React, {
+  useContext,
+  useState,
+  useRef,
+  useEffect,
+  useCallback,
+} from "react";
 import {
   Text,
   StyleSheet,
@@ -12,96 +18,15 @@ import {
 } from "react-native";
 import AuthContext from "../context/AuthContext";
 
-import { useNavigation, useFocusEffect } from '@react-navigation/native';
-
-// import * as Device from "expo-device";
-// import * as Notifications from "expo-notifications";
-
-// Notifications.setNotificationHandler({
-//   handleNotification: async () => ({
-//     shouldShowAlert: true,
-//     shouldPlaySound: true,
-//     shouldSetBadge: false,
-//   }),
-// });
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
 function Index({ navigation }) {
-
   const scaleValue = useRef(new Animated.Value(1)).current;
   const logo = require("../assets/logo.jpg");
   const { token } = useContext(AuthContext);
   const [isPressed, setIsPressed] = useState(false);
   const [isPressedFast, setIsPressedFast] = useState(false);
   // ********************************
-  // const sendNotification = async () => {
-  //   console.log("send notification");
-  //   const message = {
-  //     to: expoPushToken,
-  //     sound: "default",
-  //     title: "شغلت النوتيفيكيشن يا صيع يا صيع",
-  //     body: "بس فرونت بس بصراحه",
-  //   };
-  //   await fetch("https://exp.host/--/api/v2/push/send", {
-  //     method: "POST",
-  //     headers: {
-  //       host: "exp.host",
-  //       accept: "application/json",
-  //       "accept-encoding": "gzip, deflate",
-  //       "content-type": "application/json",
-  //     },
-  //     body : JSON.stringify(message)
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   console.log("registration");
-  //   registerForPushNotificationsAsync()
-  //     .then((token) => {
-  //       console.log("token is ", token);
-  //       setExpoPushToken(token);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
-
-  // async function registerForPushNotificationsAsync() {
-  //   let token;
-
-  //   if (Platform.OS === "android") {
-  //     await Notifications.setNotificationChannelAsync("default", {
-  //       name: "default",
-  //       importance: Notifications.AndroidImportance.MAX,
-  //       vibrationPattern: [0, 250, 250, 250],
-  //       lightColor: "#FF231F7C",
-  //     });
-  //   }
-
-  //   if (Device.isDevice) {
-  //     const { status: existingStatus } =
-  //       await Notifications.getPermissionsAsync();
-  //     let finalStatus = existingStatus;
-  //     if (existingStatus !== "granted") {
-  //       const { status } = await Notifications.requestPermissionsAsync();
-  //       finalStatus = status;
-  //     }
-  //     if (finalStatus !== "granted") {
-  //       alert("Failed to get push token for push notification!");
-  //       return;
-  //     }
-  //     // Learn more about projectId:
-  //     // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
-  //     token = (
-  //       await Notifications.getExpoPushTokenAsync({
-  //         projectId: "f67db72d-7652-45fc-80c0-70a27f8c5fae",
-  //       })
-  //     ).data;
-  //     console.log(token);
-  //   } else {
-  //     alert("Must use physical device for Push Notifications");
-  //   }
-
-  //   return token;
-  // }
-
   // ********************************
 
   // animation
@@ -132,8 +57,10 @@ function Index({ navigation }) {
   useFocusEffect(
     useCallback(() => {
       const timer = setTimeout(() => {
-        token ? navigation.navigate("ReqHospital") : navigation.navigate("SigninScreen");
-      }, 4000);
+        token
+          ? navigation.navigate("ReqHospital")
+          : navigation.navigate("SigninScreen");
+      }, 2000);
 
       return () => clearTimeout(timer); // Clean up the timer on unmount
     }, [token, navigation])
@@ -143,64 +70,9 @@ function Index({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor={"#76b49f"} color={"white"} />
-      {/* <Text style={styles.title}>Hompital</Text> */}
       {/* <Animated.View style={animatedStyle}> */}
       <Image style={styles.image} source={logo} resizeMode="contain" />
       {/* </Animated.View> */}
-
-      
-      {/* {token ? (
-        <Pressable
-          onPress={() => {
-            navigation.navigate("ReqHospital");
-          }}
-          onPressIn={() => setIsPressed(true)}
-          onPressOut={() => setIsPressed(false)}
-        >
-          <Text
-            style={[
-              styles.button,
-              { backgroundColor: isPressed ? "#9abf4d" : "#76b49f" },
-            ]}
-          >
-            طلب مستشفي
-          </Text>
-        </Pressable>
-      ) : (
-        <>
-          <Pressable
-            onPress={() => {
-              navigation.navigate("SigninScreen");
-            }}
-            onPressIn={() => setIsPressed(true)}
-            onPressOut={() => setIsPressed(false)}
-          >
-            <Text
-              style={[
-                styles.button,
-                { backgroundColor: isPressed ? "#9abf4d" : "#76b49f",},
-              ]}
-            >
-              تسجيل الدخول
-            </Text>
-          </Pressable>
-
-          <Text
-            style={[
-              styles.fastEnter,
-              { color: isPressedFast ? "#9abf4d" : "black" },
-            ]}
-            onPress={() => {
-              navigation.navigate("ReqHospital");
-            }}
-            onPressIn={() => setIsPressedFast(true)}
-            onPressOut={() => setIsPressedFast(false)}
-          >
-           دخول كزائر 
-          </Text>
-        </>
-      )} */}
-      {/* <Button title="send notification" onPress={sendNotification} /> */}
     </SafeAreaView>
   );
 }
@@ -214,13 +86,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     paddingTop: StatusBar.currentHeight,
-    // paddingBottom: 20,
   },
   title: {
     color: "#3573f9",
     fontSize: 30,
     fontWeight: "bold",
-    // marginTop: 25,
   },
   text: {
     color: "#071355",
@@ -232,14 +102,11 @@ const styles = StyleSheet.create({
   image: {
     height: 350,
     width: 350,
-    // marginTop: -30,
   },
   button: {
     paddingHorizontal: 30,
     paddingVertical: 10,
     borderRadius: 10,
-    // fontWeight: "bold",
-    // backgroundColor: "#900",
     color: "white",
     fontSize: 20,
     width: 300,
@@ -253,11 +120,9 @@ const styles = StyleSheet.create({
     elevation: 10,
     textAlign: "center",
     marginVertical: 30,
-    
   },
   fastEnter: {
     fontSize: 18,
     fontWeight: "400",
-    
   },
 });
